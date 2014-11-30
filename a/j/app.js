b6b9f8ae2696e99662809b9ca08fec9b35d25471
Natasha Vlahakis <natasha.vlahakis@gmail.com>
2014-11-30 14:50:46 -0400

    Added Masonry.js

--- a/j/app.js
+++ a/j/app.js
@@ -4,3 +4,9 @@ $(document).foundation();
 
 // Hack to get off-canvas .menu-icon to fire on iOS
 $('.menu-icon').click(function(){ false });
+
+// Masonry
+$('#masonryContainer').masonry({  
+  itemSelector: '.masonry-brick',
+  columnWidth: 480
+});    
